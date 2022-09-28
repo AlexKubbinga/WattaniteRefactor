47fe6a482809607d9016002313dffbc32f84d9e6
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-09-28 17:58:29 +0100

    testing refactor merge

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import MainBox from './components/main-box/main-box';
 import React from 'react';
 
 function App() {
-	return (
-		<div className='App'>
-			<MainBox></MainBox>
-		</div>
-	);
+  // test refactorAlex
+  return (
+    <div className="App">
+      <MainBox></MainBox>
+    </div>
+  );
 }
 
 export default App;
